88795352b9fab856fefa0072ccb95d964e7af39e
eblogavno <dzhus1111@gmail.com>
2015-07-12 16:54:14 +0300

    added tagsEvents model

---
+++ basic/models/TagsEvents.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace app\models;
+
+use Yii;
+
+/**
+ * This is the model class for table "tags_events".
+ *
+ * @property integer $tag_id
+ * @property integer $event_id
+ */
+class TagsEvents extends \yii\db\ActiveRecord
+{
+    /**
+     * @inheritdoc
+     */
+    public static function tableName()
+    {
+        return 'tags_events';
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function rules()
+    {
+        return [
+            [['tag_id', 'event_id'], 'integer']
+        ];
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function attributeLabels()
+    {
+        return [
+            'tag_id' => 'Tag ID',
+            'event_id' => 'Event ID',
+        ];
+    }
+}
