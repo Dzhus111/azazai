35037e4f282df5c41c91498e23ea189f906a0981
dzhus111 <djus_mix@ukr.net>
2016-06-19 22:20:38 +0300

    add update event template

---
+++ basic/views/events/edit_event.php
@@ -0,0 +1,22 @@
+<?php
+
+use yii\helpers\Html;
+
+
+/* @var $this yii\web\View */
+/* @var $model app\models\Events */
+
+$this->title = 'Обновление события';
+$this->registerJsFile(Yii::$app->request->baseUrl . '/js/bootstrap-tagsinput.js',  ['position' => yii\web\View::POS_END]);
+$this->registerJsFile(Yii::$app->request->baseUrl . '/js/add-new-form.js',  ['position' => yii\web\View::POS_END]);
+$this->registerCssFile(Yii::$app->request->baseUrl . '/css/bootstrap-tagsinput.css');
+?>
+
+<div class="events-create">
+    <h1><?= Html::encode($this->title) ?></h1>
+    <?= $this->render('_form', [
+        'model'     => $event,
+        'tagsStr'   => $tagsStr
+//        'errors' => $errors
+    ]) ?>
+</div>
