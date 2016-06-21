bca78c15b3f6852fb2d994ad46fd79f3930f2f3a
dzhus111 <djus_mix@ukr.net>
2016-06-21 23:02:22 +0300

    add function updateTags

--- basic/models/Tags.php
+++ basic/models/Tags.php
@@ -60,4 +60,89 @@ class Tags extends \yii\db\ActiveRecord
             ->where(['tags_events.event_id' => $eventId])
             ->all();
     }
+
+    public function updateTags($tagsStr, $event)
+    {
+        $tags = explode(",", $tagsStr);
+        $existTags = self::find()->where(['tag_name'=>$tags])->all();
+
+        if(!empty($existTags)){
+            $existsTagIds = array();
+            $existsTagNames = array();
+
+            foreach ($existTags as $existTag){
+                $existsTagNames[] = $existTag->tag_name;
+                $existsTagIds[] = $existTag->tag_id;
+            }
+
+            $assignedTags = TagsEvents::find()->where(['event_id' => $event->event_id])->all();
+
+            $_assignedTags = array();
+
+            foreach($assignedTags as $_tag){
+                $_assignedTags[] = $_tag->tag_id;
+            }
+
+            $excessTags = array_diff($_assignedTags, $existsTagIds);
+            $needToAssign = array_diff($existsTagIds, $_assignedTags);
+
+            if(!empty($excessTags)){
+                self::updateAllCounters(['events_count' => -1], ['tag_id' => $excessTags]);
+                TagsEvents::deleteAll('tag_id IN (' . implode(',', $excessTags). ') AND event_id = '.$event->event_id);
+            }
+
+            if(!empty($needToAssign)){
+                Tags::updateAllCounters(['events_count' => 1], ['tag_id' => $needToAssign]);
+
+                foreach($needToAssign as $addedTag){
+                    $_tagsEvents = new TagsEvents;
+                    $_tagsEvents->tag_id = $addedTag;
+                    $_tagsEvents->event_id = $event->event_id;
+                    $_tagsEvents->save(false);
+                }
+
+            }
+
+            $notExistsTagsNames = array_diff($tags, $existsTagNames);
+
+            if(!empty($notExistsTagsNames)){
+                foreach($notExistsTagsNames as $newTag){
+                    $tagsEvents = new TagsEvents;
+                    $tagsModel = new Tags();
+                    $tagsModel->tag_name = $newTag;
+                    $tagsModel->events_count = 1;
+                    $tagsModel->save(false);
+                    $tagsEvents->tag_id = $tagsModel->tag_id;
+                    $tagsEvents->event_id = $event->event_id;
+                    $tagsEvents->save(false);
+                }
+            }
+
+        }else{
+            $assignedTags = TagsEvents::find()->where(['event_id' => $event->event_id])->all();
+
+            $_assignedTags = array();
+
+            foreach($assignedTags as $_tag){
+                $_assignedTags[] = $_tag->tag_id;
+            }
+
+            if(!empty($_assignedTags)){
+                self::updateAllCounters(['events_count' => -1], ['tag_id' => $_assignedTags]);
+
+                TagsEvents::deleteAll('tag_id IN (' . implode(',', $_assignedTags). ') AND event_id = '.$event->event_id);
+            }
+
+            foreach($tags as $newTag){
+                $tagsEvents = new TagsEvents;
+                $tagsModel = new Tags();
+                $tagsModel->tag_name = $newTag;
+                $tagsModel->events_count = 1;
+                $tagsModel->save(false);
+                $tagsEvents->tag_id = $tagsModel->tag_id;
+                $tagsEvents->event_id = $event->event_id;
+                $tagsEvents->save(false);
+            }
+        }
+    }
 }
